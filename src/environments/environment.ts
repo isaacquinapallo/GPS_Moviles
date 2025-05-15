dd02e655a64892a881496c9c911d363df84eee62
isaacquinapallo <151254893+isaacquinapallo@users.noreply.github.com>
2025-05-15 16:37:13 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-  apiKey: "AIzaSyAXHJm3yfStFkCSz2FWISRwfbe_sis9BOI",
-  authDomain: "agr1-a52f1.firebaseapp.com",
-  projectId: "agr1-a52f1",
-  storageBucket: "agr1-a52f1.firebasestorage.app",
-  messagingSenderId: "706399491025",
-  appId: "1:706399491025:web:53963b7d880249271306f0",
-  measurementId: "G-3EZX8B9NCG"
+  apiKey: "TusCredenciales",
+  authDomain: "TusCredenciales",
+  projectId: "TusCredenciales",
+  storageBucket: "TusCredenciales",
+  messagingSenderId: "TusCredenciales",
+  appId: "TusCredenciales",
+  measurementId: "TusCredenciales"
   }
 };
 
